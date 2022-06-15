d01ef71faee0b8ee95b52fb24e64402f95757d2d
Lastly1999 <Lastly1999@163.com>
2022-06-15 17:03:17 +0800

    Request.java

---
+++ src/main/java/com/example/easyadminserviceframework/config/Request.java
@@ -0,0 +1,79 @@
+@Component
+public class HttpClientUtil {
+
+    @Autowired
+    private CloseableHttpClient httpClient;
+
+    public String get(String url) {
+        CloseableHttpResponse response = null;
+        BufferedReader in;
+        String result = "";
+        try {
+            // 创建http请求，请求设置，请求参数
+            HttpGet httpGet = new HttpGet(url);
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000).setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+            httpGet.setConfig(requestConfig);
+            httpGet.addHeader("Content-type", "application/json; charset=utf-8");
+            httpGet.setHeader("Accept", "application/json");
+            // 执行http请求
+            response = httpClient.execute(httpGet);
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            // 获取操作系统对应的换行符
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+            result = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != response) {
+                    // 释放连接
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    public String post(String url, Object param) {
+        CloseableHttpResponse response = null;
+        BufferedReader in;
+        String result = "";
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000).setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+            httpPost.setConfig(requestConfig);
+            httpPost.addHeader("Content-type", "application/json; charset=utf-8");
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setEntity(new StringEntity(JSON.toJSONString(param), Charset.forName("UTF-8")));
+            response = httpClient.execute(httpPost);
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+            result = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
